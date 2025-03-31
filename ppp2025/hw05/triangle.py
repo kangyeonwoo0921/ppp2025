d428f68ddc837a437d6cc9c5d9346210caebdc76
Yeon woo Kang <a33072041@gmail.com>
2025-03-31 15:22:26 +0900

    Update triangle.py

--- ppp2025/hw05/triangle.py
+++ ppp2025/hw05/triangle.py
@@ -3,4 +3,4 @@ import math
 n =11
 
 for i in range(n):
-    print(math.radians(i), math.sin(i), math.cos(i), math.tan(i))
+    print(math.radians(i), math.sin(math.radians(i)), math.cos(math.radians(i)), math.tan(math.radians(i)))
